922f7b2d0a7e558d4e867a2a26920bc24ab4a549
shesh nath <sheshnath.kumar@ityx.de>
2022-07-04 21:29:14 +0530

    cache test added

---
+++ SixTTests/BookCabTest/Tests/CacheTest.swift
@@ -0,0 +1,39 @@
+//
+//  CacheTest.swift
+//  SixTTests
+//
+//  Created by ityx  on 04/07/22.
+//
+
+import XCTest
+@testable import SixT
+
+class CacheTest: XCTestCase {
+
+    var cache:ImageCache!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.cache = ImageCache()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCacheSet() throws {
+        let testBundle = Bundle(for: type(of: self))
+        let image = UIImage(named: "mini_cab", in: testBundle, with: nil)!
+        let urlString = "https://cdn.sixt.io/codingtask/images/mini.png"
+        self.cache.setImage(with: urlString, image: image)
+        XCTAssertTrue(self.cache.getImage(with: urlString) != nil, "setting cache image against key(url) not working")
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
